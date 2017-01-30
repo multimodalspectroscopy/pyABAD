733961711d6f4db6d631c2f58a8b1b041f8f6a83
Josh Buckland <buck06191@gmail.com>
2017-01-30 14:06:28 +0000

    setup.py modified

--- setup.py
+++ setup.py
@@ -1,17 +1,33 @@
+# -*- coding: utf-8 -*-
+
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
+    package_list = find_packages()
 except ImportError:
     from distutils.core import setup
+    package_list = ['pyabad', 'pyabad.data_creation']
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 config = {
     'description': 'Automatic Broadband NIRS Artefact Detection',
+    'long_description': readme,
     'author': 'Joshua Russell-Buckland',
     'url': 'https://github.com/buck06191/pyABAD',
     'download_url': 'https://github.com/buck06191/pyABAD/archive/master.zip',
     'author_email': 'joshua.russell-buckland.15@ucl.ac.uk',
     'version': '0.1.0',
-    'install_requires': ['nose'],
-    'packages': ['NAME'],
+    'license': license,
+    'install_requires': required,
+    'packages': package_list,
     'scripts': [],
     'name': 'pyABAD'
 }
