74ecbdc019eb72d366a0adfb4b0f11a54001b32d
Josh Buckland <buck06191@gmail.com>
2017-01-30 17:06:49 +0000

    remove ghpages extension for now

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,8 +34,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.coverage',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.githubpages']
+    'sphinx.ext.ifconfig']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
